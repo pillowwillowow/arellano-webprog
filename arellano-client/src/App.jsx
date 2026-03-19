170b1800addd825d53d1653553ea0bf7ef41fb97
arellano <arellanoyenyen@gmail.com>
2026-03-19 21:07:11 +0800

    lab-act1

--- arellano-client/src/App.jsx
+++ arellano-client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Welcome to My React App!</h1>
+      <h1>Welcome to My React App!</h1><br />
       <p>
         Name: Yentin Jade A. Arellano <br />
         Email: arellanoyenyen@gmail.com <br />
