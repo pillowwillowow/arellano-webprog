d1904f548cb616d9c3b26e32aed759405ade67e3
arellano <arellanoyenyen@gmail.com>
2026-05-21 23:47:28 +0800

    production

--- arellano-client/src/constants.js
+++ arellano-client/src/constants.js
@@ -1,5 +1,5 @@
-const HOST = `${import.meta.env.VITE_API_URL}`;
-
-export default {
-    HOST,
+const constants = {
+  HOST: "https://arellano-webprog.onrender.com/",
 };
+
+export default constants;
