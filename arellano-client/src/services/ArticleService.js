da61d41ac827ba0bd0525898a3a51c3b94f8086f
arellano <arellanoyenyen@gmail.com>
2026-05-26 14:53:08 +0800

    lab-act7

--- arellano-client/src/services/ArticleService.js
+++ arellano-client/src/services/ArticleService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import constants from '../constants';
 
 const API = axios.create({
-    baseURL: constants.HOST,
+  baseURL: `${constants.HOST}/api/users`,
 });
 
 // Fetch articles
