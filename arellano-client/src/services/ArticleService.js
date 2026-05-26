92678f0adaf4330ad8eefc9bf480cf89a0366272
arellano <arellanoyenyen@gmail.com>
2026-05-26 15:28:50 +0800

    lab-act7

--- arellano-client/src/services/ArticleService.js
+++ arellano-client/src/services/ArticleService.js
@@ -1,17 +1,24 @@
-import axios from 'axios';
-import constants from '../constants';
+import axios from "axios";
+import constants from "../constants";
 
 const API = axios.create({
   baseURL: `${constants.HOST}/api/articles`,
 });
 
 // Fetch articles
-export const fetchArticles = () => API.get('/articles');
+export const fetchArticles = () => API.get("/");
+
 // Create article
-export const createArticle = (article) => API.post('/articles', article);
+export const createArticle = (article) => API.post("/", article);
+
 // Update article
-export const updateArticle = (id, article) => API.put(`/articles/${id}`, article);
+export const updateArticle = (id, article) =>
+  API.put(`/${id}`, article);
+
 // Delete article
-export const deleteArticle = (id) => API.delete(`/articles/${id}`);
+export const deleteArticle = (id) =>
+  API.delete(`/${id}`);
+
 // Get article by slug
-export const getArticleBySlug = (slug) => API.get(`/articles/slug/${slug}`);
+export const getArticleBySlug = (slug) =>
+  API.get(`/slug/${slug}`);
